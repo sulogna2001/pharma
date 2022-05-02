d42027008800ae238c35fa04c975464670a1d24e
sulogna2001 <roysulogna2001@gmail.com>
2022-05-02 17:11:44 +0530

    Added

--- controller/Buyer/buyer.js
+++ controller/Buyer/buyer.js
@@ -4,26 +4,23 @@ const { isValidObjectId } = require("mongoose");
 const getBuyer = async (req, res) => {
   try {
     const decodedValue = req.user;
-    console.log(decodedValue);
+    // console.log(decodedValue);
 
     if (!decodedValue)
       return res.status(403).json("No Authorization Token Sent");
-  
-    const id=decodedValue._id;
-    console.log(id);
+    const id = decodedValue._id;
 
     if (!isValidObjectId(id)) {
       return res.status(403).json("Invalid User");
     }
+    console.log(id);
 
-    // console.log(id);
-
-    const buyer = await Buyers.findOne({ _id:id });
+    const buyer = await Buyers.findOne({ _id: id });
     if (!buyer) return res.status(403).json("No Such User has Logged In");
 
-    return res.status(200).json(buyer);
+    // return res.status(200).json(req.user);
 
-    // return res.status(200).json(buyer);
+    return res.status(200).json(buyer);
   } catch (error) {
     return res.status(500).json(error.message);
   }
--- controller/Seller/seller.js
+++ controller/Seller/seller.js
@@ -4,7 +4,7 @@ const { isValidObjectId } = require("mongoose");
 const getSeller = async (req, res) => {
   try {
     const decodedValue = req.user;
-    console.log(decodedValue);
+    // console.log(decodedValue);
 
     if (!decodedValue)
       return res.status(403).json("No Authorization Token Sent");
@@ -18,9 +18,9 @@ const getSeller = async (req, res) => {
     const seller = await Sellers.findOne({ _id: id });
     if (!seller) return res.status(403).json("No Such User has Logged In");
 
-    return res.status(200).json(req.user);
+    // return res.status(200).json(req.user);
 
-    // return res.status(200).json(buyer);
+    return res.status(200).json(seller);
   } catch (error) {
     return res.status(500).json(error.message);
   }
